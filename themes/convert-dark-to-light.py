ad07ee9961dd465b6f16b6be549d6e2c1d2a2e1d
Andrew D'Amario <51063733+andrewaclear@users.noreply.github.com>
2022-10-23 19:31:51 -0400

    feat: better colour calculations and improvements

--- themes/convert-dark-to-light.py
+++ themes/convert-dark-to-light.py
@@ -19,32 +19,7 @@ for line in dark:
       red = int(parts[i][1:3], 16)
       green = int(parts[i][3:5], 16)
       blue = int(parts[i][5:7], 16)
-      # s = red + green + blue
-      # if s < 0x55*3:
-      #   inc = 0x1b
-      #   if s < 0x11*3:
-      #     inc = 0xdd
-      #   elif s < 0x44*3:
-      #     inc = 0x99
-      #   red = min(red+inc, 0xff)
-      #   green = min(green+inc, 0xff)
-      #   blue = min(blue+inc, 0xff)
-      # else:
-      #   # dec = 0x50
-      #   # if s < 0x66*3:
-      #   #   dec = 0x0b
-      #   # elif s < 0xb0*3:
-      #   #   dec = 0x2b
-      #   # elif s < 0xd7:
-      #   #   dec = 0x33
-      #   dec = 0x1b
-      #   if s > 0xd0*3:
-      #     dec = 0xd0
-      #   elif s > 0xb0*3:
-      #     dec = 0x90
-      #   red = max(red-dec, 0x00)
-      #   green = max(green-dec, 0x00)
-      #   blue = max(blue-dec, 0x00)
+      
       s = red + green + blue
       k = [red, green, blue]
       m = int(median(k))
@@ -56,7 +31,7 @@ for line in dark:
           diff = 0x2e
         else:
           # whites, greys -> black
-          diff = int(diff/1.1)
+          diff = int(diff/1.2)
         red = max(red-diff, 0x00)
         green = max(green-diff, 0x00)
         blue = max(blue-diff, 0x00)        
